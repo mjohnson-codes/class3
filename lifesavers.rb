a38cd2d5d5112431fb584ea93e96070593f334b9
mjohnson-codes <mjohnson@trialnetwork.com>
2015-05-26 20:13:08 -0400

    lifesavers

---
+++ lifesavers.rb
@@ -0,0 +1,22 @@
+puts("Time to sort my lifesavers!\n\n")
+
+roll = ["red", "green", "green", "yellow", "red"]
+
+puts("Starting roll = ")
+puts(roll)
+
+
+tmp = roll.pop
+roll.push(tmp)
+
+puts("\n\nEnding roll = ")
+
+puts(roll)
+sorted_roll.push(roll[0])
+sorted_roll.push(roll[4])
+sorted_roll.push(roll[1])
+sorted_roll.push(roll[2])
+sorted_roll.push(roll{3])
+
+
+  
